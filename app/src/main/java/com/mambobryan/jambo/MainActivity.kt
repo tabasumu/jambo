719ef88f2fbbc2a38a275faafde89501b93ec380
MamboBryan <mambobryan@gmail.com>
2022-06-09 17:01:03 +0300

    origins

---
+++ app/src/main/java/com/mambobryan/jambo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mambobryan.jambo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.mambobryan.jambo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+    }
+}
