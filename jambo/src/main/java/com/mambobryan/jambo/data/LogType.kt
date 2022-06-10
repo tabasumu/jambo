01509bd020c1af5d33c3ae64489c67d2557f408d
MamboBryan <mambobryan@gmail.com>
2022-06-10 11:48:12 +0300

    adds logging type

---
+++ jambo/src/main/java/com/mambobryan/jambo/data/LogType.kt
@@ -0,0 +1,16 @@
+package com.mambobryan.jambo.data
+
+/**
+ * Jambo
+ * @author Mambo Bryan
+ * @email mambobryan@gmail.com
+ * Created 6/10/22 at 11:46 AM
+ */
+enum class LogType{
+    INFO,
+    VERBOSE,
+    ERROR,
+    DEBUG,
+    WARN,
+    ASSERT
+}
