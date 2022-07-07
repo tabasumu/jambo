0c55d811d59e0a1b70e437edbf16562e3be0281f
johnGachihi <johngachihi3@gmail.com>
2022-07-07 17:35:19 +0300

    enable sharing log (#13)

--- jambo/src/main/java/com/tabasumu/jambo/ui/MoreBottomSheet.kt
+++ jambo/src/main/java/com/tabasumu/jambo/ui/MoreBottomSheet.kt
@@ -2,6 +2,7 @@ package com.tabasumu.jambo.ui
 
 import android.app.Dialog
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.tabasumu.jambo.R
 import com.tabasumu.jambo.databinding.BottomSheetMoreBinding
 import com.tabasumu.jambo.helpers.getColor
 import com.tabasumu.jambo.helpers.getFullTag
@@ -61,7 +63,7 @@ class MoreBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.toolbarMore.inflateMenu(R.menu.menu_bottom_sheet_more)
 
         lifecycleScope.launchWhenResumed {
             viewModel.selectedLog.collectLatest {
@@ -69,13 +71,32 @@ class MoreBottomSheet : BottomSheetDialogFragment() {
                     binding.apply {
                         toolbarMore.title = it.getFullTag()
                         toolbarMore.setNavigationOnClickListener { dismiss() }
+                        toolbarMore.setOnMenuItemClickListener { menuItem ->
+                            when (menuItem.itemId) {
+                                R.id.action_share -> {
+                                    shareLogMessage(it.message)
+                                    true
+                                }
+                                else -> false
+                            }
+                        }
+
                         tvMore.text = it.message
 
                         layoutMoreTag.setBackgroundColor(Color.parseColor(it.getColor().second))
                     }
             }
         }
+    }
 
+    private fun shareLogMessage(message: String) {
+        val shareIntent = Intent.createChooser(Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, message)
+            type = "text/plain"
+        }, null)
+
+        startActivity(shareIntent)
     }
 
     override fun onDismiss(dialog: DialogInterface) {
